068363ffb1f880b33ec9ce15dad7aea49b7bc825
scch1002 <chrisschwan8009@hotmail.com>
2016-01-13 02:13:44 -0600

    Changed to jagged arrays and corrected Echelon Form

--- Math/Matrix.cs
+++ Math/Matrix.cs
@@ -8,40 +8,60 @@ namespace Math
 {
     public class Matrix
     {
-        private static void RowReplacement(double[,] matrix, int rowNew, int rowWith, double scaler)
+        private static double[][] InitilizeMatrix(int rows, int columns)
         {
-            for(int column = 0; column < matrix.GetLength(1); column++)
+            var matrix = new double[rows][];
+            for (int row = 0; row < rows; row++)
             {
-                matrix[rowNew, column] += (matrix[rowWith, column] * scaler);
+                matrix[row] = new double[columns];
+            }
+            return matrix;
+        }
+
+        private static double[][] CopyMatrix(double[][] matrix)
+        {
+            var result = InitilizeMatrix(matrix.GetLength(0), matrix.First().GetLength(0));
+            for (int copyRow = 0; copyRow < matrix.GetLength(0); copyRow++)
+            {
+                for (int copyColumn = 0; copyColumn < matrix[copyRow].GetLength(0); copyColumn++)
+                {
+                    result[copyRow][copyColumn] = matrix[copyRow][copyColumn];
+                }
+            }
+            return result;
+        }
+
+        private static void RowReplacement(double[][] matrix, int rowNew, int rowWith, double scaler)
+        {
+            for(int column = 0; column < matrix[rowWith].GetLength(0); column++)
+            {
+                matrix[rowNew][column] += (matrix[rowWith][column] * scaler);
             }
         }
 
-        private static void RowInterchange(double[,] matrix, int rowFrom, int rowTo)
+        private static void RowInterchange(double[][] matrix, int rowFrom, int rowTo)
         {
-            double intermediate = 0;
-            for(int column = 0; column < matrix.GetLength(1); column++)
+            double[] intermediate;
+            intermediate = matrix[rowTo];
+            matrix[rowTo] = matrix[rowFrom];
+            matrix[rowFrom] = intermediate;
+        }
+
+        private static void RowScale(double[][] matrix, int row, double scaler)
+        {
+            for (int column = 0; column < matrix.First().GetLength(0); column++)
             {
-                intermediate = matrix[rowTo, column];
-                matrix[rowTo, column] = matrix[rowFrom, column];
-                matrix[rowFrom, column] = intermediate;
+                matrix[row][column] *= scaler;
             }
         }
 
-        private static void RowScale(double[,] matrix, int row, double scaler)
-        {
-            for (int column = 0; column < matrix.GetLength(1); column++)
-            {
-                matrix[row, column] *= scaler;
-            }
-        }
-
-        private static void RowToEchelonForm(double[,] matrix, int effectColumn, int startRow)
+        private static void RowToEchelonForm(double[][] matrix, int effectColumn, int startRow)
         { 
-            if (matrix[startRow, effectColumn] == 0)
+            if (matrix[startRow][effectColumn] == 0)
             {
                 for(int row = startRow; row < matrix.GetLength(0); row++)
                 {
-                    if (matrix[row, effectColumn] != 0)
+                    if (matrix[row][effectColumn] != 0)
                     {
                         RowInterchange(matrix, row, startRow);
                         break;
@@ -51,71 +71,108 @@ namespace Math
 
             for(int row = startRow + 1; row < matrix.GetLength(0); row++)
             {
-                if (matrix[row, effectColumn] != 0)
+                if (matrix[row][effectColumn] != 0)
                 {
-                    RowScale(matrix, row, 1.0/matrix[row, effectColumn]);
-                    RowScale(matrix, row, matrix[startRow, effectColumn]);
+                    RowScale(matrix, row, matrix[startRow][effectColumn] / matrix[row][effectColumn]);
                     RowReplacement(matrix, row, startRow, -1);
                 }
             }
         }
 
-        private static void RowToReducedEchelonForm(double[,] matrix, int effectColumn, int startRow)
+        private static void RowToReducedEchelonForm(double[][] matrix, int effectColumn, int startRow)
         {
             RowToEchelonForm(matrix, effectColumn, startRow);
 
             for (int row = startRow - 1; row > -1; row--)
             {
-                if (matrix[row, effectColumn] != 0)
+                if (matrix[row][effectColumn] != 0)
                 {
-                    RowScale(matrix, row, 1.0 / matrix[row, effectColumn]);
-                    RowScale(matrix, row, matrix[startRow, effectColumn]);
+                    RowScale(matrix, row, 1.0 / matrix[row][effectColumn]);
+                    RowScale(matrix, row, matrix[startRow][effectColumn]);
                     RowReplacement(matrix, row, startRow, -1);
                 }
             }
         }
 
+        private static void InterchangeZerosToBottom(double[][] matrix, int currentRow)
+        {
+            var subMatrix = new double[matrix.GetLength(0) - currentRow][];
+            for(int row = 0; row < matrix.GetLength(0) - currentRow; row++)
+            {
+                subMatrix[row] = matrix[row + currentRow];
+            }
+
+            Array.Sort(subMatrix, MoreLeadingZerosThan);
+
+            for (int row = 0; row < matrix.GetLength(0) - currentRow; row++)
+            {
+                matrix[row + currentRow] = subMatrix[row];
+            }
+        }
+
+        private static int MoreLeadingZerosThan(double[] first, double[] second)
+        {
+            int firstCount = 0;
+            int secondCount = 0;
+            foreach(var value in first)
+            {
+                if (value != 0)
+                {
+                    break;
+                }
+                firstCount++;
+            }
+            foreach (var value in second)
+            {
+                if (value != 0)
+                {
+                    break;
+                }
+                secondCount++;
+            }
+            return firstCount < secondCount
+                ? -1
+                : firstCount == secondCount
+                ? 0
+                : 1;
+        }
+
         //ref(A)
-        public static double[,] EchelonForm(double[,] matrix)
+        public static double[][] EchelonForm(double[][] matrix)
         {
             var rows = matrix.GetLength(0);
-            var columns = matrix.GetLength(1);
+            var columns = matrix.First().GetLength(0);
 
-            var result = new double[rows, columns];
+            var result = CopyMatrix(matrix);
 
-            for(int copyRow = 0; copyRow < rows; copyRow++)
+            int pivotColumn;
+            for(pivotColumn = 0; pivotColumn < rows; pivotColumn++)
             {
-                for(int copyColumn = 0; copyColumn < columns; copyColumn++)
-                {
-                    result[copyRow, copyColumn] = matrix[copyRow, copyColumn];
-                }
+                RowToEchelonForm(result, pivotColumn, pivotColumn);
             }
 
-            int column = 0;
-            for(int row = 0; row < rows; row++)
+            int zeroCount = 0;
+            while(result[rows - 2][zeroCount] == 0)
             {
-                RowToEchelonForm(result, column, row);
-                column++;
+                zeroCount++;
             }
-            
+
+            if (zeroCount == pivotColumn - 1)
+            {
+                RowScale(result, pivotColumn - 1, matrix[pivotColumn - 2][pivotColumn - 1] / matrix[pivotColumn - 1][pivotColumn - 1]);
+                RowReplacement(result, pivotColumn - 1, pivotColumn - 1, -1);
+            }
+
             return result;
         }
 
         //rref(A)
-        public static double[,] ReducedEchelonForm(double[,] matrix)
+        public static double[][] ReducedEchelonForm(double[][] matrix)
         {
             var rows = matrix.GetLength(0);
             var columns = matrix.GetLength(1);
 
-            var result = new double[rows, columns];
-
-            for (int copyRow = 0; copyRow < rows; copyRow++)
-            {
-                for (int copyColumn = 0; copyColumn < columns; copyColumn++)
-                {
-                    result[copyRow, copyColumn] = matrix[copyRow, copyColumn];
-                }
-            }
+            var result = CopyMatrix(matrix);
 
             int column = 0;
             for (int row = 0; row < rows; row++)
@@ -127,7 +184,7 @@ namespace Math
             column = 0;
             for (int row = 0; row < rows; row++)
             {
-                RowScale(result, row, 1.0 / result[row, column]);
+                RowScale(result, row, 1.0 / result[row][column]);
                 column++;
             }
 
@@ -135,41 +192,91 @@ namespace Math
         }
 
         //Ax=b
-        public static double[] LinearTranformation(double[,] matrix, double[] vector)
+        public static double[] LinearTranformation(double[][] matrix, double[] vector)
         {
             var result = new double[vector.GetLength(0)];
             for(int row = 0; row < matrix.GetLength(0); row++)
             {
-                for (int column = 0; column < matrix.GetLength(1); column++)
+                for (int column = 0; column < matrix[row].GetLength(0); column++)
                 {
-                    result[row] += matrix[row, column] * vector[column];
+                    result[row] += matrix[row][column] * vector[column];
                 }
             }
             return result;
         }
 
         //AB=C
-        public static double[,] MatrixMultiply(double[,] matrixA, double[,] matrixB)
+        public static double[][] MatrixMultiply(double[][] matrixA, double[][] matrixB)
         {
             var rows = matrixA.GetLength(0);
-            var columns = matrixB.GetLength(1);
+            var columns = matrixB.First().GetLength(0);
 
-            double[,] matrix_Result = new double[rows, columns];
+            var matrix_Result = InitilizeMatrix(rows, columns);
 
-            for(var row = 0; row < rows; row++)
+            for (var row = 0; row < rows; row++)
             {
                 var vector = new double[columns];
                 for(int column = 0; column < columns; column++)
                 {
-                    vector[column] = matrixB[column, row];
+                    vector[column] = matrixB[column][row];
                 }
                 var result = LinearTranformation(matrixA, vector);
                 for (int column = 0; column < columns; column++)
                 {
-                    matrix_Result[column, row] = result[column];
+                    matrix_Result[column][row] = result[column];
                 }
             }
             return matrix_Result;
+        }
+        
+        //Linear Independent
+        public static bool IsLinearlyIndependent(double[][] matrix)
+        {
+            if (matrix.First().GetLength(0) > matrix.GetLength(0))
+            {
+                return false;
+            }
+
+            bool containsZeroVector = true;
+            for (int column = 0; column < matrix.First().GetLength(0); column++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    if (matrix[row][column] != 0)
+                    {
+                        containsZeroVector = false;
+                        continue;
+                    }
+                }
+                if (containsZeroVector)
+                {
+                    return false;
+                }
+                containsZeroVector = true;
+            }
+
+            var result = CopyMatrix(matrix);
+
+            var echelonForm = EchelonForm(result);
+
+            var rowAllZeros = true;
+            for(int row = 0; row < echelonForm.GetLength(0); row++)
+            {
+                for(int column = 0; column < echelonForm.First().GetLength(0); column++)
+                {
+                    if (echelonForm[row][column] != 0)
+                    {
+                        rowAllZeros = false;
+                    }
+                }
+                if (rowAllZeros)
+                {
+                    return false;
+                }
+                rowAllZeros = true;
+            }
+            
+            return true;
         }
     }
 }
--- MathTests/MatrixTests.cs
+++ MathTests/MatrixTests.cs
@@ -10,47 +10,64 @@ namespace MathTests
         [TestMethod]
         public void Matrix_EchelonForm()
         {
-            var matrix = new double[3, 3]
+            var matrix = new double[3][]
             {
-                {3, 5, 9 },
-                {8, 1, 5 },
-                {9, 2, 9 }
+                new double[] {3, 5, 9 },
+                new double[] {8, 1, 5 },
+                new double[] {9, 2, 9 }
             };
 
             var result = Matrix.EchelonForm(matrix);
 
-            Assert.AreEqual(result[1, 0], 0);
-            Assert.AreEqual(result[2, 0], 0);
-            Assert.AreEqual(result[2, 1], 0);
+            Assert.AreEqual(0, result[1][0]);
+            Assert.AreEqual(0, result[2][0]);
+            Assert.AreEqual(0, result[2][1]);
+
+            var matrixDependent = new double[3][]
+            {
+                new double[] { 3, 9, 2 },
+                new double[] { 3, 9, 8 },
+                new double[] { 3, 9, 6 }
+            };
+
+            var dependentResult = Matrix.EchelonForm(matrixDependent);
+
+            Assert.AreEqual(0, dependentResult[1][0]);
+            Assert.AreEqual(0, dependentResult[1][1]);
+            Assert.AreEqual(0, dependentResult[2][0]);
+            Assert.AreEqual(0, dependentResult[2][1]);
+            Assert.AreEqual(0, dependentResult[2][2]);
         }
 
         [TestMethod]
         public void Matrix_ReducedEchelonForm()
         {
-            var matrixA = new double[3, 3] { { 3, 4, 5 },
-                                            { 12, 5, 3 },
-                                            { 89, 1, 9 }};
+            var matrixA = new double[3][] {
+                new double [] { 3, 4, 5 },
+                new double [] { 12, 5, 3 },
+                new double [] { 89, 1, 9 }};
 
             var result = Matrix.ReducedEchelonForm(matrixA);
 
-            Assert.AreEqual(1, result[0, 0]);
-            Assert.AreEqual(0, result[0, 1]);
-            Assert.AreEqual(0, result[0, 2]);
-            Assert.AreEqual(0, result[1, 0]);
-            Assert.AreEqual(1, result[1, 1]);
-            Assert.AreEqual(0, result[1, 2]);
-            Assert.AreEqual(0, result[2, 0]);
-            Assert.AreEqual(0, result[2, 1]);
-            Assert.AreEqual(1, result[2, 2]);
+            Assert.AreEqual(1, result[0][0]);
+            Assert.AreEqual(0, result[0][1]);
+            Assert.AreEqual(0, result[0][2]);
+            Assert.AreEqual(0, result[1][0]);
+            Assert.AreEqual(1, result[1][1]);
+            Assert.AreEqual(0, result[1][2]);
+            Assert.AreEqual(0, result[2][0]);
+            Assert.AreEqual(0, result[2][1]);
+            Assert.AreEqual(1, result[2][2]);
         }
 
         [TestMethod]
         public void Matrix_LinearTransformation()
         {
-            var matrixA = new double[4, 4] { { 5, 3, 8, 80 },
-                                        { 7, 1, 9, 1 },
-                                        { 50, 21, 1, 35 },
-                                        { 6, 4, 56, 96 }};
+            var matrixA = new double[4][] {
+                new double [] { 5, 3, 8, 80 },
+                new double [] { 7, 1, 9, 1 },
+                new double [] { 50, 21, 1, 35 },
+                new double [] { 6, 4, 56, 96 }};
             var vector = new double[] { 6, 7, 9, 4 };
             var transformationResultExpected = new double[] { 443, 134, 596, 952 };
 
@@ -65,28 +82,81 @@ namespace MathTests
         [TestMethod]
         public void Matrix_Multiply()
         {
-            var matrixA = new double[4, 4] { { 5, 3, 8, 80 },
-                                        { 7, 1, 9, 1 },
-                                        { 50, 21, 1, 35 },
-                                        { 6, 4, 56, 96 }};
-            var matrixB = new double[4, 4] { { 6, 3, 0, 0 },
-                                        { 7, 1, 0, 0 },
-                                        { 9, 0, 1, 0 },
-                                        { 4, 2, 0, 0 }};
-            var matrixC = new double[4, 4] { { 443, 178, 8, 0 },
-                                        { 134, 24, 9, 0 },
-                                        { 596, 241, 1, 0 },
-                                        { 952, 214, 56, 0 }};
+            var matrixA = new double[4][] { 
+                new double [] { 5, 3, 8, 80 },
+                new double [] { 7, 1, 9, 1 },
+                new double [] { 50, 21, 1, 35 },
+                new double [] { 6, 4, 56, 96 }};
+            var matrixB = new double[4][] {
+                new double [] { 6, 3, 0, 0 },
+                new double [] { 7, 1, 0, 0 },
+                new double [] { 9, 0, 1, 0 },
+                new double [] { 4, 2, 0, 0 }};
+            var matrixC = new double[4][] {
+                new double [] { 443, 178, 8, 0 },
+                new double [] { 134, 24, 9, 0 },
+                new double [] { 596, 241, 1, 0 },
+                new double [] { 952, 214, 56, 0 }};
 
             var matrix_Result = Matrix.MatrixMultiply(matrixA, matrixB);
 
-            for (var row = 0; row < matrixA.Rank; row++)
+            for (var row = 0; row < matrixC.GetLength(0); row++)
             {
-                for (var column = 0; column < matrixA.GetLength(row); column++)
+                for (var column = 0; column < matrixC[row].GetLength(0); column++)
                 {
-                    Assert.AreEqual(matrixC[row, column], matrix_Result[row, column]);
+                    Assert.AreEqual(matrixC[row][column], matrix_Result[row][column]);
                 }
             }
+        }
+
+        [TestMethod]
+        public void Matrix_LinearlyIndependent_More_Columns_Then_Rows()
+        {
+            var matrix = new double[3][];
+            matrix[0] = new double[4];
+            matrix[1] = new double[4];
+            matrix[2] = new double[4];
+            var independent = Matrix.IsLinearlyIndependent(matrix);
+            Assert.AreEqual(false, independent);
+        }
+
+        [TestMethod]
+        public void Matrix_LinearlyIndependent_Column_Of_All_Zeros()
+        {
+            var matrix = new double[4][]
+            {
+                new double [] { 4, 0, 3, 4 },
+                new double [] { 8, 0, 89, 5 },
+                new double [] { 7, 0, 78, 4 },
+                new double [] { 65, 0, 63, 5 }
+            };
+            var independent = Matrix.IsLinearlyIndependent(matrix);
+            Assert.AreEqual(false, independent);
+        }
+
+        [TestMethod]
+        public void Matrix_LinearlyIndependent_AugmentedMatrixTest()
+        {
+            var matrixIndependent = new double[4][]
+            {
+                new double [] { 6, 45, 36, 8 },
+                new double [] { 7, 21, 6, 9 },
+                new double [] { 12, 3, 8, 9 },
+                new double [] { 21, 6, 41, 6 }
+            };
+
+            var independent = Matrix.IsLinearlyIndependent(matrixIndependent);
+            Assert.AreEqual(true, independent);
+
+            var matrixDependent = new double[3][]
+            {
+                new double [] { 3, 9, 2 },
+                new double [] { 3, 9, 8 },
+                new double [] { 3, 9, 6 }
+            };
+
+            independent = Matrix.IsLinearlyIndependent(matrixDependent);
+            Assert.AreEqual(false, independent);
         }
     }
 }
